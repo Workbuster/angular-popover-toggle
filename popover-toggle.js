e323234ea69eece5a8b75a36794e2d8008e741fa
jskogelin <jonathan@workbuster.com>
2015-09-03 14:23:34 +0200

    Close popover on popover-close click. Only one active popover allowed at any time.

--- popover-toggle.js
+++ popover-toggle.js
@@ -4,6 +4,9 @@
     var POPOVER_SHOW = 'popoverToggleShow';
     var POPOVER_HIDE = 'popoverToggleHide';
 
+    var triggerHide;
+    var openEl;
+
     var module = angular.module('popoverToggle', ['ui.bootstrap']);
 
     module.config(['$tooltipProvider', function($tooltipProvider) {
@@ -13,7 +16,26 @@
         $tooltipProvider.setTriggers(triggers);
     }]);
 
-    module.directive('popoverToggle', ['$timeout', function($timeout) {
+    module.directive('popoverClose', [function () {
+        return {
+            restrict: 'A',
+            link: link
+        };
+
+        function link($scope, $element, $attrs) {
+            $element.on('click', function (e) {
+                if (angular.element(e.target).closest('.popover').length === 0
+                    && !angular.element(e.target).attr('popover')
+                    && angular.element(e.target).closest('[popover]').length === 0) {
+
+                    triggerHide()
+
+                }
+            });
+        }
+    }]);
+
+    module.directive('popoverToggle', ['$timeout', '$parse', function($timeout, $parse) {
         return {
             restrict: 'A',
             link: link
@@ -23,14 +45,22 @@
             $attrs.popoverTrigger = POPOVER_SHOW;
 
             $scope.$watch($attrs.popoverToggle, function(newValue) {
+                if (openEl !== $element && triggerHide) {
+                    triggerHide();
+                }
                 $timeout(function(){
                     if(newValue) {
+                        openEl = $element;
                         $element.triggerHandler(POPOVER_SHOW);
+                        triggerHide = function () {
+                            $element.triggerHandler(POPOVER_HIDE);
+                            $parse($attrs.popoverToggle).assign($scope, false);
+                        }
                     } else {
                         $element.triggerHandler(POPOVER_HIDE);
                     }
                 });
-            })
+            });
         }
     }]);
 
